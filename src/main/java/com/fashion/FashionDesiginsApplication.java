ca2157181d4a8caacb90f33662412735c601f7ab
JITHENDRA SAI RAMESH MATTA <110710275+klu-2100031674@users.noreply.github.com>
2024-12-11 17:54:26 +0530

    Update FashionDesiginsApplication.java

--- src/main/java/com/fashion/FashionDesiginsApplication.java
+++ src/main/java/com/fashion/FashionDesiginsApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class FashionDesiginsApplication {
-
+	@GetMapping("/hi)
+	public String example(){
+		return "hi";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FashionDesiginsApplication.class, args);
 	}
